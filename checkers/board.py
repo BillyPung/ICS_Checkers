f9d727123a94061cb62a3745c0fdf15ff6c8b2dc
Billy Peng <62238461+BillyPung@users.noreply.github.com>
2021-05-06 01:00:55 +0800

    test

--- checkers/board.py
+++ checkers/board.py
@@ -1,23 +1,29 @@
 import pygame
-from .constants import BLACK, ROWS, RED, SQUARE_SIZE,COLS, WHITE
+
+from .constants import BLACK, ROWS, RED, SQUARE_SIZE, COLS, WHITE
 from .piece import Piece
+
+
 class Board:
     def __init__(self):
-        self.board=[]
-        self.selected_piece=None
-        self.red_left = self.white_left = 12
+        self.board = []
+        self.selected_piece = None
+        self.red_left = 12
+        self.white_left = 12
         self.red_kings = self.white_kings = 0
         self.create_board()
-    def draw_squares(self,win):
+
+    def draw_squares(self, win):
         win.fill(BLACK)
         for row in range(ROWS):
-            for col in range(row%2, COLS, 2):
-                pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            for col in range(row % 2, COLS, 2):
+                pygame.draw.rect(win, RED, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
     def create_board(self):
         for row in range(ROWS):
             self.board.append([])
             for col in range(COLS):
-                if col % 2 == ((row +  1) % 2):
+                if col % 2 == ((row + 1) % 2):
                     if row < 3:
                         self.board[row].append(Piece(row, col, WHITE))
                     elif row > 4:
@@ -26,6 +32,7 @@ class Board:
                         self.board[row].append(0)
                 else:
                     self.board[row].append(0)
+
     def draw(self, win):
         self.draw_squares(win)
         for row in range(ROWS):
@@ -34,6 +41,12 @@ class Board:
                 if piece != 0:
                     piece.draw(win)
 
-
-
-
+    def skip_left(self, start, end, direction, color, target_col, skipped=[]):
+        possible_moves = {}
+        last_move = []
+        for i in range(start, end, direction):
+            if target_col < 0:
+                break
+            current_move = self.board[i][target_col]
+            if current_move:
+                pass
